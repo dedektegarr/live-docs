a252693b3837666c046aab57aa147baffc5cff63
Dedek Tegar Apriyandi <dedektegarr@gmail.com>
2024-08-10 23:25:29 +0700

    feat: adding liveblocks for collaborative users

---
+++ src/components/CollaborativeRoom.tsx
@@ -0,0 +1,34 @@
+import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense";
+import React from "react";
+import Loader from "./Loader";
+import Header from "./Header";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Editor } from "./editor/Editor";
+
+const CollaborativeRoom = () => {
+  return (
+    <div>
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<Loader />}>
+          <div className="collaborative-room">
+            <Header>
+              <div className="flex w-fit items-center justify-center gap-2">
+                <p className="document-title">This is a fake document title</p>
+              </div>
+
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </Header>
+            <Editor />
+          </div>
+        </ClientSideSuspense>
+      </RoomProvider>
+    </div>
+  );
+};
+
+export default CollaborativeRoom;
