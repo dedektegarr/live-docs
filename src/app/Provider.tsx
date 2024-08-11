3bd4867edf082ab0884b0762daa52b31d38a4f55
Dedek Tegar Apriyandi <dedektegarr@gmail.com>
2024-08-11 17:40:09 +0700

    feat: mentions feature

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -7,15 +7,27 @@ import {
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import Loader from "@/components/Loader";
-import { getClerkUser } from "@/lib/actions/user.actioins";
+import { getClerkUser, getDocumentUsers } from "@/lib/actions/user.actioins";
+import { useUser } from "@clerk/nextjs";
 
 export function Provider({ children }: { children: ReactNode }) {
+  const { user: clerkUser } = useUser();
+
   return (
     <LiveblocksProvider
       authEndpoint="/api/liveblocks-auth"
       resolveUsers={async ({ userIds }) => {
         const users = await getClerkUser({ userIds });
         return users;
+      }}
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+          text,
+        });
+
+        return roomUsers;
       }}
     >
       <RoomProvider id="my-room">
