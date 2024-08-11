8f340bd75289b17682e0e9654514fc7782e31dc3
Dedek Tegar Apriyandi <dedektegarr@gmail.com>
2024-08-11 09:54:21 +0700

    feat: implement create document functionality

--- src/components/CollaborativeRoom.tsx
+++ src/components/CollaborativeRoom.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense";
 import React from "react";
 import Loader from "./Loader";
@@ -7,27 +9,25 @@ import { Editor } from "./editor/Editor";
 
 const CollaborativeRoom = () => {
   return (
-    <div>
-      <RoomProvider id="my-room">
-        <ClientSideSuspense fallback={<Loader />}>
-          <div className="collaborative-room">
-            <Header>
-              <div className="flex w-fit items-center justify-center gap-2">
-                <p className="document-title">This is a fake document title</p>
-              </div>
+    <RoomProvider id="my-room">
+      <ClientSideSuspense fallback={<Loader />}>
+        <div className="collaborative-room">
+          <Header>
+            <div className="flex w-fit items-center justify-center gap-2">
+              <p className="document-title">This is a fake document title</p>
+            </div>
 
-              <SignedOut>
-                <SignInButton />
-              </SignedOut>
-              <SignedIn>
-                <UserButton />
-              </SignedIn>
-            </Header>
-            <Editor />
-          </div>
-        </ClientSideSuspense>
-      </RoomProvider>
-    </div>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </Header>
+          <Editor />
+        </div>
+      </ClientSideSuspense>
+    </RoomProvider>
   );
 };
 
